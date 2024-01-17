e7dba5d9312451ace71c79b5f681858ead716ff3
yukong <kyb062666@gmail.com>
2024-01-17 14:43:20 +0900

    [CHORE] 초기 프로젝트 셋팅

---
+++ app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.sesac.aoao.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
