96c394c0c6076d264ff65726c20b2735ee9d018c
kimeunseo58 <alan0221@naver.com>
2024-01-18 19:48:32 +0900

    [FEAT] 하단바 / 상점 레이아웃 및 액티비티 생성

---
+++ app/src/main/java/kr/sesac/aoao/android/RecyclerViewAdapter_Market.kt
@@ -0,0 +1,57 @@
+package kr.sesac.aoao.android
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import kr.sesac.aoao.android.model.ItemData
+import org.w3c.dom.Text
+
+class RecyclerViewAdapter_Market(private val itemList: List<ItemData>, private val context: Context) :
+    RecyclerView.Adapter<RecyclerViewAdapter_Market.MarketItemViewHolder>()
+{
+    class MarketItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imgItem: ImageView = itemView.findViewById(R.id.market_item_img)
+        val itemName :TextView = itemView.findViewById(R.id.market_item_name)
+        val price : TextView = itemView.findViewById(R.id.market_item_money)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_dino_market_item, parent, false)
+        return RecyclerViewAdapter_Market.MarketItemViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun onBindViewHolder(holder: MarketItemViewHolder, position: Int) {
+        val itemData = itemList[position]
+        val drawableResId = when (itemData.name) {
+            "item_food" -> R.drawable.item_food
+            "item_medicine" -> R.drawable.item_medicine
+            "item_play" -> R.drawable.item_play
+            "item_love" -> R.drawable.item_love
+            else -> R.drawable.item_set
+        }
+        val item_name = when(itemData.name){
+            "item_food" -> "사료"
+            "item_medicine" -> "약품"
+            "item_play" -> "놀이"
+            "item_love" -> "애정"
+            else -> "1 세트"
+        }
+        val backgroundColor_green = ContextCompat.getColor(context, R.color.raise_dino_green)
+        val backgroundDrawable = holder.imgItem.background as GradientDrawable
+        backgroundDrawable.setColor(backgroundColor_green)
+        holder.imgItem.setImageDrawable(ContextCompat.getDrawable(context, drawableResId))
+        holder.itemName.text = item_name
+        holder.price.text = itemData.price.toString()
+    }
+}
---
+++ app/src/main/java/kr/sesac/aoao/android/RecyclerViewAdapter_RaiseDino.kt
@@ -0,0 +1,44 @@
+package kr.sesac.aoao.android
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import kr.sesac.aoao.android.model.ItemData
+
+class RecyclerViewAdapter_RaiseDino(private val itemList: List<ItemData>, private val context: Context) :
+    RecyclerView.Adapter<RecyclerViewAdapter_RaiseDino.RaiseDinoItemViewHolder>() {
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RaiseDinoItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_dino_item_button, parent, false)
+        return RaiseDinoItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RaiseDinoItemViewHolder, position: Int) {
+        val itemData = itemList[position]
+        holder.numTextView.text = itemData.num.toString()
+        val drawableResId = when (itemData.name) {
+            "item_food" -> R.drawable.item_food
+            "item_medicine" -> R.drawable.item_medicine
+            "item_play" -> R.drawable.item_play
+            else -> R.drawable.item_love
+        }
+        val backgroundColor_green2 = ContextCompat.getColor(context, R.color.raise_dino_dark_green)
+        val backgroundDrawable = holder.numTextView.background as GradientDrawable
+        backgroundDrawable.setColor(backgroundColor_green2)
+        holder.imgItem.setImageDrawable(ContextCompat.getDrawable(context, drawableResId))
+    }
+
+    class RaiseDinoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgItem: ImageView = itemView.findViewById(R.id.img_item)
+        val numTextView: TextView = itemView.findViewById(R.id.num_item)
+    }
+}
