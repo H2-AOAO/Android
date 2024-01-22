8588898c523f8e1cc6185d0f199cb1d92917fe92
yukong <kyb062666@gmail.com>
2024-01-22 21:05:44 +0900

    [FEAT] todolist 와 diary 스위칭 구현

---
+++ app/src/main/java/kr/sesac/aoao/android/HomeActivity2.kt
@@ -0,0 +1,54 @@
+package kr.sesac.aoao.android
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+import androidx.fragment.app.Fragment
+import kr.sesac.aoao.android.databinding.ActivityCalendar2Binding
+
+/**
+ * @since 2024.01.19 ~
+ * @author 김유빈, 최정윤
+ */
+class HomeActivity2 : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCalendar2Binding
+
+    private val calendarLayoutId = R.id.calendar
+    private val contentLayoutId = R.id.content
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCalendar2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // calendar 초기화
+        switchFragment(calendarLayoutId, CalendarFragment())
+
+        // switch 체크 이벤트
+        setOnCheckedEvent()
+        switchFragment(contentLayoutId, TodolistFragment())
+    }
+
+    private fun setOnCheckedEvent(
+    ) {
+        val contentTitle: TextView = binding.todo
+        val switchingButton: SwitchCompat = binding.switchingButton
+        switchingButton.setOnCheckedChangeListener { p0, isChecked ->
+            if (isChecked) {
+                contentTitle.text = "다이어리"
+                switchFragment(contentLayoutId, DiaryFragment())
+            } else {
+                contentTitle.text = "투두"
+                switchFragment(contentLayoutId, TodolistFragment())
+            }
+        }
+    }
+
+    private fun switchFragment(layoutId: Int, fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(layoutId, fragment)
+        fragmentTransaction.commit()
+    }
+}
