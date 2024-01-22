7b92598682495fdcdfb2c20b7856ed4ed5d97827
yukong <kyb062666@gmail.com>
2024-01-23 00:42:44 +0900

    [FEAT] 수정 및 초기화 버튼에 대한 로직 처리

--- app/src/main/java/kr/sesac/aoao/android/DiaryFragment.kt
+++ app/src/main/java/kr/sesac/aoao/android/DiaryFragment.kt
@@ -1,18 +1,31 @@
 package kr.sesac.aoao.android
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import kr.sesac.aoao.android.databinding.FragmentDiaryBinding
 import kr.sesac.aoao.android.model.TodayViewModel
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 
 class DiaryFragment : Fragment() {
 
     private lateinit var binding : FragmentDiaryBinding
     private lateinit var todayViewModel: TodayViewModel
+
+    private lateinit var year: String
+    private lateinit var month: String
+    private lateinit var dayOfMonth: String
+    private lateinit var fname: String
+
+    private var isEdit = false
+    private val userId = "userId"
 
     override fun onCreateView(
         inflater: LayoutInflater, // 뷰를 생성하는 객체
@@ -23,6 +36,8 @@ class DiaryFragment : Fragment() {
         todayViewModel = ViewModelProvider(requireActivity())[TodayViewModel::class.java]
 
         observeSelectedDate()
+        setUpdateButtonClickEvent()
+        setDeleteButtonClockEvent()
         return binding.root
     }
 
@@ -32,8 +47,67 @@ class DiaryFragment : Fragment() {
                 todayViewModel.dayOfMonth.observe(viewLifecycleOwner) { dayOfMonth ->
                     val today = "$year / $month / $dayOfMonth"
                     binding.date.text = today
+
+                    this.year = year.toString()
+                    this.month = month.toString()
+                    this.dayOfMonth = dayOfMonth.toString()
+
+                    writeDiary()
                 }
             }
+        }
+    }
+
+    private fun setUpdateButtonClickEvent() {
+        binding.updateBtn.setOnClickListener {
+            if (isEdit) {
+                isEdit = false
+                binding.diaryEditText.isEnabled = false
+                binding.updateBtn.text = "수정"
+                saveDiary(fname, binding.diaryEditText.text.toString())
+            } else {
+                isEdit = true
+                binding.diaryEditText.isEnabled = true
+                binding.updateBtn.text = "저장"
+            }
+        }
+    }
+
+    private fun setDeleteButtonClockEvent() {
+        binding.deleteBtn.setOnClickListener {
+            binding.diaryEditText.setText("")
+            saveDiary(fname, "")
+        }
+    }
+
+    private fun writeDiary() {
+        fname = "$userId$year-$month-$dayOfMonth.txt"
+        val fileInputStream: FileInputStream
+        try {
+            fileInputStream = requireContext().openFileInput(fname)
+            val fileData = ByteArray(fileInputStream.available())
+            fileInputStream.read(fileData)
+            fileInputStream.close()
+            binding.diaryEditText.setText(String(fileData))
+        } catch (e: FileNotFoundException) {
+            binding.diaryEditText.setText("")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    // 달력 내용 추가
+    @SuppressLint("WrongConstant")
+    fun saveDiary(readDay: String?, content: String) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = requireContext().openFileOutput(readDay,
+                AppCompatActivity.MODE_NO_LOCALIZED_COLLATORS
+            )
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
         }
     }
 }
