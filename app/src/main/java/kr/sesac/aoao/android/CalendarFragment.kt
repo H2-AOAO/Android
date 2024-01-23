e6dec7774ef2475959c6c83ba187297acdec6f5a
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-23 11:45:08 +0900

    [FEATURE] 투두리스트 및 다이어리 스위칭 fragment 방식으로 변환 (#12)
    * [FEAT] todolist 와 diary 스위칭 구현
    
    * [FEAT] 캘린더 날짜 선택 시 다이어리 날짜 변경
    
    * [REFACTOR] 컨텐츠 잘리지 않도록 높이 조절
    
    * [FEAT] 스위칭 버튼 선택 시 목표 설정 버튼 수정
    
    * [FEAT] 저장 및 초기화 버튼 추가
    
    * [REFACTOR] 날짜 데이터 분할하여 전달
    
    * [FEAT] 수정 및 초기화 버튼에 대한 로직 처리
    
    * [REFACTOR] 기능별 주석 설명 추가
    
    * [REFACTOR] 기존 HomeActivity 대체
    
    * [REFACTOR] layout 이름 변경

---
+++ app/src/main/java/kr/sesac/aoao/android/CalendarFragment.kt
@@ -0,0 +1,47 @@
+package kr.sesac.aoao.android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import kr.sesac.aoao.android.databinding.FragmentCalendarBinding
+import kr.sesac.aoao.android.model.TodayViewModel
+
+/**
+ * @since 2024.01.22
+ * @author 김유빈
+ */
+class CalendarFragment: Fragment() {
+
+    private lateinit var binding : FragmentCalendarBinding
+    private lateinit var todayViewModel: TodayViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, // 뷰를 생성하는 객체
+        container: ViewGroup?, // 생성할 뷰(자식 뷰)가 들어갈 부모 뷰
+        savedInstanceState: Bundle? // 이전 프래그먼트 객체에서 전달된 데이터(번들)
+    ): View {
+        binding = FragmentCalendarBinding.inflate(layoutInflater)
+        todayViewModel = ViewModelProvider(requireActivity())[TodayViewModel::class.java]
+        setOnDateChangeEvent()
+        return binding.root
+    }
+
+    /**
+     * 선택한 데이터 정보 ViewModel 담아 전달
+     * @since 2024.01.22
+     * @author 김유빈
+     */
+    private fun setOnDateChangeEvent() {
+        val calendar: CalendarView = binding.calendarView
+
+        calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            todayViewModel.year.value = year
+            todayViewModel.month.value = month + 1
+            todayViewModel.dayOfMonth.value = dayOfMonth
+        }
+    }
+}
