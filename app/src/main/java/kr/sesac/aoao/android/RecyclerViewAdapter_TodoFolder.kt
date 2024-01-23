33dc42a021145e3921adbf4651301821d4b56cac
yukong <kyb062666@gmail.com>
2024-01-23 14:07:39 +0900

    [REFACTOR] main 충돌 해결

--- app/src/main/java/kr/sesac/aoao/android/RecyclerViewAdapter_TodoFolder.kt
+++ app/src/main/java/kr/sesac/aoao/android/RecyclerViewAdapter_TodoFolder.kt
@@ -5,16 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kr.sesac.aoao.android.model.TodoFolderData
 
 class RecyclerViewAdapter_TodoFolder(
     private val folders: List<TodoFolderData>,
-    private val context: Context)
+    private val context: Context,
+    private val onItemClick: (TodoFolderData) -> Unit)
     : RecyclerView.Adapter<RecyclerViewAdapter_TodoFolder.TodoFolderViewHolder>()
 {
     class TodoFolderViewHolder(folder: View) : RecyclerView.ViewHolder(folder) {
         val name: TextView = folder.findViewById(R.id.todoFolderName)
+        val item: ConstraintLayout = folder.findViewById(R.id.todoFolderItem)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoFolderViewHolder {
@@ -29,5 +32,10 @@ class RecyclerViewAdapter_TodoFolder(
     override fun onBindViewHolder(holder: TodoFolderViewHolder, position: Int) {
         val folder = folders[position]
         holder.name.text = folder.name
+
+        // 아이템 클릭 이벤트 처리
+        holder.item.setOnClickListener {
+            onItemClick(folder)
+        }
     }
 }
--- app/src/main/java/kr/sesac/aoao/android/TodoFolderActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/TodoFolderActivity.kt
@@ -3,12 +3,16 @@ package kr.sesac.aoao.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kr.sesac.aoao.android.databinding.ActivityTodoFolderBinding
+import kr.sesac.aoao.android.databinding.BottomSheetDialogTodoFolderBinding
 import kr.sesac.aoao.android.model.TodoFolderData
 
 class TodoFolderActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityTodoFolderBinding
+    private lateinit var bottomSheetBinding : BottomSheetDialogTodoFolderBinding
+
     private lateinit var folders : List<TodoFolderData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +30,20 @@ class TodoFolderActivity : AppCompatActivity() {
             TodoFolderData("루틴"),
             TodoFolderData("생활"),
         )
-        val adapter = RecyclerViewAdapter_TodoFolder(folders, this)
+        val adapter = RecyclerViewAdapter_TodoFolder(folders, this) { clickedFolder ->
+            showBottomSheetDialog(clickedFolder)
+        }
         recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun showBottomSheetDialog(clickedFolder: TodoFolderData) {
+        val dialog = BottomSheetDialog(this)
+        bottomSheetBinding = BottomSheetDialogTodoFolderBinding.inflate(layoutInflater)
+        dialog.setContentView(bottomSheetBinding.root)
+
+        bottomSheetBinding.bottomSheetTitle.text = clickedFolder.name
+
+        dialog.show()
     }
 }
