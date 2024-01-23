44f686c9187f4ee4c23b3dd9b740782dee115295
yukong <kyb062666@gmail.com>
2024-01-23 14:18:06 +0900

    [FEAT] 투두리스트 폴더 삭제 기능 구현

--- app/src/main/java/kr/sesac/aoao/android/TodoFolderActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/TodoFolderActivity.kt
@@ -2,6 +2,7 @@ package kr.sesac.aoao.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kr.sesac.aoao.android.databinding.ActivityTodoFolderBinding
@@ -12,8 +13,10 @@ class TodoFolderActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityTodoFolderBinding
     private lateinit var bottomSheetBinding : BottomSheetDialogTodoFolderBinding
+    private lateinit var adapter : RecyclerViewAdapter_TodoFolder
+    private lateinit var dialog : BottomSheetDialog
 
-    private lateinit var folders : List<TodoFolderData>
+    private lateinit var folders : MutableList<TodoFolderData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,7 @@ class TodoFolderActivity : AppCompatActivity() {
             TodoFolderData("루틴"),
             TodoFolderData("생활"),
         )
-        val adapter = RecyclerViewAdapter_TodoFolder(folders, this) { clickedFolder ->
+        adapter = RecyclerViewAdapter_TodoFolder(folders, this) { clickedFolder ->
             showBottomSheetDialog(clickedFolder)
         }
         recyclerView.adapter = adapter
@@ -38,12 +41,33 @@ class TodoFolderActivity : AppCompatActivity() {
     }
 
     private fun showBottomSheetDialog(clickedFolder: TodoFolderData) {
-        val dialog = BottomSheetDialog(this)
+        dialog = BottomSheetDialog(this)
         bottomSheetBinding = BottomSheetDialogTodoFolderBinding.inflate(layoutInflater)
         dialog.setContentView(bottomSheetBinding.root)
 
         bottomSheetBinding.bottomSheetTitle.text = clickedFolder.name
+        setUpdateButtonEventInBottomSheetDialog(clickedFolder)
+        setDeleteButtonEventInBottomSheetDialog(clickedFolder)
 
         dialog.show()
+    }
+
+    private fun setUpdateButtonEventInBottomSheetDialog(folder: TodoFolderData) {
+        bottomSheetBinding.updateButton.setOnClickListener {
+
+        }
+    }
+
+    private fun setDeleteButtonEventInBottomSheetDialog(folder: TodoFolderData) {
+        bottomSheetBinding.deleteButton.setOnClickListener {
+            Thread {
+                folders.remove(folder)
+                runOnUiThread {
+                    adapter.notifyDataSetChanged()
+                    Toast.makeText(this, "삭제되었습니다.", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+            }.start()
+        }
     }
 }
