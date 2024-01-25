9364feff442780abee45a280ed487e2a287dcb9a
kimeunseo58 <alan0221@naver.com>
2024-01-25 09:03:22 +0900

    [FEATURE] 다이노 생성 페이지_2

--- app/src/main/java/kr/sesac/aoao/android/dino/ui/NewDinoActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/dino/ui/NewDinoActivity.kt
@@ -3,7 +3,11 @@ package kr.sesac.aoao.android.dino.ui
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kr.sesac.aoao.android.R
@@ -17,6 +21,9 @@ import kr.sesac.aoao.android.market.ui.MarketActivity
  */
 class NewDinoActivity : AppCompatActivity(){
     lateinit var binding: ActivityNewDinoBinding
+    var dinoColor : String = "green"
+    var dinoName : String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -25,6 +32,11 @@ class NewDinoActivity : AppCompatActivity(){
         }
         binding = ActivityNewDinoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnPink.setOnClickListener { checkColor("pink") }
+        binding.btnBlue.setOnClickListener { checkColor("blue") }
+        binding.btnYellow.setOnClickListener { checkColor("yellow") }
+        binding.btnPurple.setOnClickListener { checkColor("purple") }
+        binding.btnGreen.setOnClickListener { checkColor("green") }
 
         binding.btnSetDino.setOnClickListener {
             //다이노 업데이트 통신 서비스
@@ -32,6 +44,18 @@ class NewDinoActivity : AppCompatActivity(){
             val intent = Intent(this, RaiseDinoActivity::class.java)
             startActivity(intent)
         }
+    }
 
+    private fun checkColor(color : String) {
+        val colorList : List<String> = listOf(
+            "pink", "blue","yellow","purple","green"
+        )
+        dinoColor = color
+        for(i in 0..4){
+            val viewName = "${colorList[i]}_check"
+            val resourceId = resources.getIdentifier(viewName, "id", packageName)
+            if (colorList[i].equals(color)) findViewById<View>(resourceId)?.visibility = View.VISIBLE
+             else findViewById<View>(resourceId)?.visibility = View.INVISIBLE
+        }
     }
 }
