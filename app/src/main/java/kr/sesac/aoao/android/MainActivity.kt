326fe9bc48c180c7b6018a4dfc4913d309903e66
yukong <kyb062666@gmail.com>
2024-01-26 12:13:40 +0900

    [REFACTOR] main 충돌 해결

--- app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
@@ -1,12 +1,28 @@
 package kr.sesac.aoao.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.sesac.aoao.android.databinding.ActivityMainBinding
+import kr.sesac.aoao.android.user.ui.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setEmailLoginButtonOnClickListener()
+
+        setContentView(binding.root)
+    }
+
+    private fun setEmailLoginButtonOnClickListener() {
+        binding.emailLoginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
