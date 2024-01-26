227ec60ddfa99296dea76a06f7a6c8d7c9c42df7
yukong <kyb062666@gmail.com>
2024-01-26 12:27:40 +0900

    [FEAT] 메인 페이지 상단바 적용

--- app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
@@ -3,6 +3,8 @@ package kr.sesac.aoao.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import kr.sesac.aoao.android.databinding.ActivityMainBinding
 import kr.sesac.aoao.android.user.ui.LoginActivity
 
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR //상태바 글자색 검은색으로
 
         setEmailLoginButtonOnClickListener()
 
