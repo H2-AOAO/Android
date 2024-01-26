8c37c1a6e65dfce408d42afc30753b5cc4da841f
yukong <kyb062666@gmail.com>
2024-01-26 11:01:05 +0900

    [REFACTOR] main 충돌 해결

--- app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
@@ -1,14 +1,7 @@
 package kr.sesac.aoao.android
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.cardview.widget.CardView
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kr.sesac.aoao.android.calendar.ui.HomeActivity
-import kr.sesac.aoao.android.dino.ui.RaiseDinoActivity
-import kr.sesac.aoao.android.friend.FriendActivity
-import kr.sesac.aoao.android.user.ui.MyPageActivity
 
 class MainActivity : AppCompatActivity() {
 
