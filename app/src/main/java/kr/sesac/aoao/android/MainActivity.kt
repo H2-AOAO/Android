7615ccf2666641abc67ec6e1a12413f8c03c1049
kyukong <92148749+kyukong@users.noreply.github.com>
2024-01-26 16:54:59 +0900

    [FEATURE] 온보딩 페이지 제작 (#38)
    * [REFACTOR] main 충돌 해결
    
    * [REFACTOR] main 충돌 해결
    
    * [FEAT] 메인 페이지 상단바 적용
    
    * [REFACTOR] 로그인 페이지 로고 위치 조절
    
    * [REFACTOR] 로그인 / 회원가입 버튼 디자인 수정

--- app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/MainActivity.kt
@@ -3,17 +3,31 @@ package kr.sesac.aoao.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.cardview.widget.CardView
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kr.sesac.aoao.android.calendar.ui.HomeActivity
-import kr.sesac.aoao.android.dino.ui.RaiseDinoActivity
-import kr.sesac.aoao.android.friend.FriendActivity
-import kr.sesac.aoao.android.user.ui.MyPageActivity
+import android.view.View
+import androidx.core.content.ContextCompat
+import kr.sesac.aoao.android.databinding.ActivityMainBinding
+import kr.sesac.aoao.android.user.ui.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR //상태바 글자색 검은색으로
+
+        setEmailLoginButtonOnClickListener()
+
+        setContentView(binding.root)
+    }
+
+    private fun setEmailLoginButtonOnClickListener() {
+        binding.emailLoginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
