f75a83faf1a736d07ab46b11f94f1bf390b7021a
kimeunseo58 <alan0221@naver.com>
2024-01-28 06:33:27 +0900

    [CHORE] 하단바 완료 및 상단바 색 통일

--- app/src/main/java/kr/sesac/aoao/android/user/ui/MyPageActivity.kt
+++ app/src/main/java/kr/sesac/aoao/android/user/ui/MyPageActivity.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kr.sesac.aoao.android.R
+import kr.sesac.aoao.android.common.BottomNavigationHandler
 import kr.sesac.aoao.android.common.TokenManager
 import kr.sesac.aoao.android.databinding.ActivityMyPageBinding
 import kr.sesac.aoao.android.user.service.UserRepository
@@ -60,9 +63,15 @@ class MyPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
 
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white) //상태바 색깔 하얀색
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR //상태바 글자색 검은색
         binding = ActivityMyPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initializeViews()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        BottomNavigationHandler(this,"MypageActivity").attach(bottomNavigationView)
+
 
         accessToken = TokenManager.getAccessTokenWithTokenType(this)
         userId = TokenManager.getUserId(this).toLong()
